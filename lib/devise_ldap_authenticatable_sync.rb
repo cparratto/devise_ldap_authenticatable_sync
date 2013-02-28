139b9e2828ba994901e30d19c6768f67c33ff9dd
Christopher Parratto <cparratto@gmail.com>
2013-02-28 11:52:33 -0800

    Added all of the correct hooks for adding the user credentials.

--- lib/devise_ldap_authenticatable_sync.rb
+++ lib/devise_ldap_authenticatable_sync.rb
@@ -1,2 +1,11 @@
-module DeviseLdapAuthenticatableSync
+require 'devise'
+require 'devise_ldap_authenticatable'
+
+require 'devise_ldap_authenticatable_sync/engine'
+require 'devise_ldap_authenticatable_sync/model'
+require 'devise_ldap_authenticatable_sync/ldap_adapter'
+
+module Devise
+  mattr_accessor :ldap_sync_registrations
+  @@ldap_sync_registrations = true
 end
